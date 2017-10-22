19b5d16a2f95a3491f2e5fdfd7eb72649be2b1b3
chrisguttandin <chrisguttandin@media-codings.com>
2017-10-22 06:00:59 +0200

    chore(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,5 +1,5 @@
 module.exports = {
     lint: {
-        cmd: 'tslint -c config/tslint/src.json --project src/tsconfig.json --type-check src/**/*.ts'
+        cmd: 'tslint -c config/tslint/src.json --project src/tsconfig.json src/**/*.ts'
     }
 };

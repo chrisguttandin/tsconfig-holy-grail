63b0154e7ebd4b91dcebbbd858b08cf34065f0e3
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-28 18:47:03 +0100

    uninstall tslint

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,8 +1,6 @@
 module.exports = {
     lint: [
-        'eslint',
-        // @todo Use grunt-lint again when it support the type-check option.
-        'sh:lint'
+        'eslint'
     ],
     test: [
         'mochaTest'
--- config/grunt/sh.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    lint: {
-        cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
-    }
-};

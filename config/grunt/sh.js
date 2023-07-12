2875a8816380049e2c0617a153bc0824e2ee124f
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-07-12 14:21:17 +0200

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -3,7 +3,7 @@ module.exports = (grunt) => {
 
     return {
         'lint-config': {
-            cmd: `eslint --config config/eslint/config.json --ext .js ${fix ? '--fix ' : ''}--report-unused-disable-directives *.js config/`
+            cmd: 'npm run lint:config'
         },
         'lint-test': {
             cmd: `eslint --config config/eslint/test.json --ext .js ${fix ? '--fix ' : ''}--report-unused-disable-directives test/`
